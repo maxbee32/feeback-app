2bc3ad42b48d2fd3f3c6482ad08a25af3eaca192
Maxwell <mboakye@excenit.com>
2023-04-13 16:55:35 +0000

    schedule test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sendemailbranch:cron')->everyMinute();
+        $schedule->command('sendemailbranch:cron')->dailyAt('16:00');
        # dailyAt('16:00');
     }
 
